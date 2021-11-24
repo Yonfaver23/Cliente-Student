ebde2f94368acbbcf205b9ea567ffc7c24e97073
Faver Izquierdo <jhonfa199418@gmail.com>
2021-11-24 00:05:08 -0500

    panel administrador

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,24 +1,30 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-import { loginComponent } from './components/pages/login/login.component';
-import { AdminForumComponent } from './components/pages/admin-forum/admin-forum.component';
-import { StudentForumComponent } from './components/pages/student-forum/student-forum.component';
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+import { loginComponent } from "./components/pages/login/login.component";
+import { AdminForumComponent } from "./components/pages/admin-forum/admin-forum.component";
+import { StudentForumComponent } from "./components/pages/student-forum/student-forum.component";
+import { PanelAdminComponent } from "./components/pages/panel-admin/panel-admin.component";
 
 const routes: Routes = [
   {
-    path: '',
-    pathMatch: 'full',
+    path: "",
+    pathMatch: "full",
     component: loginComponent,
   },
   {
-    path: 'admin',
-    pathMatch: 'full',
+    path: "admin",
+    pathMatch: "full",
     component: AdminForumComponent,
   },
   {
-    path: 'studen',
-    pathMatch: 'full',
+    path: "studen",
+    pathMatch: "full",
     component: StudentForumComponent,
+  },
+  {
+    path: "admin/panel",
+    pathMatch: "full",
+    component: PanelAdminComponent,
   },
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,18 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { HeaderComponent } from './components/header/header/header.component';
-import { loginComponent } from './components/pages/login/login.component';
-import { AdminForumComponent } from './components/pages/admin-forum/admin-forum.component';
-import { StudentForumComponent } from './components/pages/student-forum/student-forum.component';
-import { FormsModule } from '@angular/forms';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { HeaderComponent } from "./components/header/header/header.component";
+import { loginComponent } from "./components/pages/login/login.component";
+import { AdminForumComponent } from "./components/pages/admin-forum/admin-forum.component";
+import { StudentForumComponent } from "./components/pages/student-forum/student-forum.component";
+import { FormsModule } from "@angular/forms";
+import { PanelAdminComponent } from "./components/pages/panel-admin/panel-admin.component";
+import { NavBarAdminComponent } from "./components/nav-bar-admin/nav-bar-admin.component";
+import { HeaderAdminComponent } from "../app/components/header-admin/header-admin.component";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { CdkAccordionModule } from "@angular/cdk/accordion";
 
 @NgModule({
   declarations: [
@@ -16,8 +21,17 @@ import { FormsModule } from '@angular/forms';
     loginComponent,
     AdminForumComponent,
     StudentForumComponent,
+    PanelAdminComponent,
+    NavBarAdminComponent,
+    HeaderAdminComponent,
   ],
-  imports: [BrowserModule, AppRoutingModule, FormsModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    CdkAccordionModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
